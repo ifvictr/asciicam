c465160fd10b62c843a472baaf5c49c756730bfa
Victor Truong <me@ifvictr.com>
2019-08-20 13:06:00 -0700

    Make styling consistent + add types for socket.io

---
+++ server/dist/constants.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var ChatEvent;
+(function (ChatEvent) {
+    ChatEvent["CONNECT"] = "connect";
+    ChatEvent["DISCONNECT"] = "disconnect";
+    ChatEvent["MESSAGE"] = "message";
+})(ChatEvent = exports.ChatEvent || (exports.ChatEvent = {}));
+//# sourceMappingURL=constants.js.map

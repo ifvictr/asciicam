b4312c0c5dd985775df36b144c99c66a131202b3
Victor Truong <me@ifvictr.com>
2019-08-20 23:51:20 -0700

    Re-add demo code

--- cli/index.ts
+++ cli/index.ts
@@ -13,4 +13,31 @@ program
 import('./commands/create').then(command => command.default(program))
 import('./commands/join').then(command => command.default(program))
 
+program
+    .command('create [passphrase]', '')
+    .command('join <roomId>', '')
+
+const opts: any = {
+    width: 1280,
+    height: 720,
+    quality: 100,
+    delay: 0,
+    // Save shots in memory
+    saveShots: false,
+    // [jpeg, png] support varies
+    output: "jpeg",
+    device: false,
+    // [location, buffer, base64]
+    callbackReturn: "buffer",
+    verbose: false
+}
+const webcam = NodeWebcam.create(opts)
+setInterval(() => {
+    webcam.capture("test", (err: any, data: any) => {
+        imageToAscii(data, (err: any, convertedImage: any) => {
+            // TODO: Optimize for realtime render
+            console.log(convertedImage)
+        })
+    })
+}, 500)
 program.parse(process.argv)

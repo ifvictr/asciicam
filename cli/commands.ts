cb7adddf9ba19ae1c36fc566c5bc14da5c154ecf
Roz Stengle <rosalind.stengle@gmail.com>
2019-08-21 22:39:47 -0700

    added audio recording

--- cli/commands.ts
+++ cli/commands.ts
@@ -3,6 +3,9 @@ var program = require('commander');
 const imageToAscii = require('image-to-ascii')
 // import NodeWebcam from "node-webcam"
 const NodeWebcam = require('node-webcam')
+var Mic = require('node-microphone')
+var wav = require('wav')
+var fs = require('fs');
 
 program
     .command('create [passphrase]')
@@ -32,17 +35,34 @@ program
             //concat: false
         }
         const webcam = NodeWebcam.create(camOpts)
+        const mic = new Mic()
 
         setInterval(() => {
             webcam.capture("test", (err: any, data: any) => {
                 imageToAscii(data, imgOpts, (err: any, convertedImage: any) => {
                     // TODO: Optimize for realtime render
                     // console.log(convertedImage + "\r\x1B[50A")
-                    console.log("\n\n" + convertedImage + "\x1B[0;0H")
-                    //console.log(computeSize(options.size))
+                    //console.log("\n\n" + convertedImage + "\x1B[0;0H")
                 })
             })
         }, 500)
+
+
+        
+        let micStream = mic.startRecording();
+        //saving to a local .wav file for now, we can sort actual wrtie stream later
+        micStream.pipe( fs.createWriteStream('./testaudio.wav') );
+        setTimeout(() => {
+            console.log('stopped recording audio');
+            mic.stopRecording();
+        }, 3000);
+        // mic.on('info', (info) => {
+        //     console.log(info);
+        // });
+        // mic.on('error', (error) => {
+        //     console.log(error);
+        // });
+        
 		});
 
 

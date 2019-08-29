8a346fddfc54329c83eb1c22b450c2546fbd2522
Victor Truong <me@ifvictr.com>
2019-08-29 15:24:25 -0700

    Remove build files

--- server/dist/constants.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ChatEvent;
-(function (ChatEvent) {
-    ChatEvent["CONNECT"] = "connect";
-    ChatEvent["DISCONNECT"] = "disconnect";
-    ChatEvent["MESSAGE"] = "message";
-})(ChatEvent = exports.ChatEvent || (exports.ChatEvent = {}));
-//# sourceMappingURL=constants.js.map
--- server/dist/server.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ChatServer_1 = require("./ChatServer");
-let app = new ChatServer_1.ChatServer().app;
-exports.app = app;
-//# sourceMappingURL=server.js.map
